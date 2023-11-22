11b13ec7009219e012f87a91b42da44ddc495e7f
GabrielMorth <gabrielmorth@usp.br>
2023-11-22 15:36:01 -0300

    próximos da versão final

--- forum/forms.py
+++ forum/forms.py
@@ -2,12 +2,12 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Disciplina, Post, Comment, Arquivo, Reply
-   
+
 class CustomUserCreationForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True, help_text='Obrigatório.')
     last_name = forms.CharField(max_length=30, required=True, help_text='Obrigatório.')
     email = forms.EmailField(max_length=254, help_text='Obrigatório. Informe um email válido.')
-    
+
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
@@ -16,31 +16,33 @@ class ArquivoForm(forms.ModelForm):
     class Meta:
         model = Arquivo
         fields = ['titulo', 'arquivo',]
-        
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'content', 'disciplina')
+        fields = ('title', 'content', 'disciplina') # novo campo
         widgets = {
-            'disciplina': forms.HiddenInput()
+            'disciplina': forms.HiddenInput() # campo oculto
         }
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('text',)
+
     def clean_text(self):
         text = self.cleaned_data.get('text')
         if not text:
             raise forms.ValidationError('O campo de texto é obrigatório.')
-        if len(text) > 1000:
-            raise forms.ValidationError('O texto não pode ter mais de 1000 caracteres.')
+        if len(text) > 500:
+            raise forms.ValidationError('O texto não pode ter mais de 500 caracteres.')
         return text
 
 class ReplyForm(forms.ModelForm):
     class Meta:
         model = Reply
         fields = ('text',)
+
     def clean_text(self):
         text = self.cleaned_data.get('text')
         if not text:
--- forum/models.py
+++ forum/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 from django.urls import reverse
 from tutorpoli.settings import TIME_ZONE
 
+
 class Disciplina(models.Model):
     name = models.CharField(max_length=200)
     horario_monitoria = models.TextField()
@@ -13,10 +14,10 @@ class Disciplina(models.Model):
     sala = models.TextField()
     nome_monitor = models.TextField()
     contador = models.IntegerField(default=0)
-
+    
     def __str__(self):
         return self.name
-    
+
 class Profile(models.Model):
     TIPOS = (
         ('A', 'Aluno'),
@@ -29,32 +30,6 @@ class Profile(models.Model):
     biografia = models.TextField(blank=True)
     tempo_ativo = models.DurationField(default=timedelta())
 
-class Post(models.Model):
-    title = models.CharField("Título", max_length=200)
-    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
-    content = models.TextField("Conteúdo")
-    post_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-    def __str__(self):
-        return self.title
-
-class Comment(models.Model):
-    post = models.ForeignKey('forum.Post', on_delete=models.CASCADE, related_name='comments')
-    disciplina = models.ForeignKey('forum.Disciplina', on_delete=models.CASCADE, related_name='comments', null=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField("Conteúdo")
-    created_date = models.DateTimeField(auto_now_add=True)
-    
-    class Meta:
-        ordering = ['-created_date']
-    def __str__(self):
-        return self.text
-    def get_absolute_url(self):
-        return reverse('post_detail', kwargs={'pk': self.post.pk})
-    
 class Arquivo(models.Model):
     TIPOS = (
         ('P', 'Prova'),
@@ -67,17 +42,48 @@ class Arquivo(models.Model):
     contador = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-class Reply(models.Model):
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='replies')
+class Post(models.Model):
+    title = models.CharField("Título", max_length=200)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE) # novo campo
+    content = models.TextField("Conteúdo")
+    post_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('forum.Post', on_delete=models.CASCADE, related_name='comments')
+    disciplina = models.ForeignKey('forum.Disciplina', on_delete=models.CASCADE, related_name='comments', null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField("Conteúdo")
     created_date = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
         ordering = ['-created_date']
 
     def __str__(self):
         return self.text
-    
+
+    def get_absolute_url(self): # novo método
+        return reverse('post_detail', kwargs={'pk': self.post.pk})
+
+class Reply(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='replies')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField("Conteúdo")
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.text
+
     def get_absolute_url(self):
         return reverse('post_detail', kwargs={'pk': self.comment.post.pk})
+

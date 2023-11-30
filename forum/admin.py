8eeef24b133c851550d17f9d8c71463267753558
Guilherme Penteado <guilhermep2003@usp.br>
2023-11-30 15:48:43 -0300

    Correções adm

--- forum/admin.py
+++ forum/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
-from .models import Disciplina, Post, Comment, Reply, Arquivo
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Profile, Disciplina, Post, Comment, Reply, Arquivo
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'tipo', 'tempo_ativo', 'biografia')
+    list_filter = ('tipo',)
+    search_fields = ['user__username', 'user__first_name', 'user__last_name']
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Disciplina)
